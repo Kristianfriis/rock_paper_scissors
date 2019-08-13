4d0b371ef367e529e51ff425c1896576aeaaf95b
Kristian Friis <friis1212@gmail.com>
2019-08-13 11:06:16 +0200

    finished the game logic in JS

--- script.js
+++ script.js
@@ -15,20 +15,47 @@ function computerPlay() {
     }
     return computerChoice;
 };
+/*Global variables to hold scores*/
+var playerScore = 0;
+var computerScore = 0;
 
+/*Function that checks whether the computer og player wins, and updates the score accordingly by 1 */
 function playRound(playerSelection, computerSelection){
-    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Scissors' ) {
-        return "You Win! Rock beats Scissors";
-    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Rock') {
-        return "You Win! Paper beats Rock";
-    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Paper') {
-        return "You Win! Scissors beats Paper";
-    } else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-        return "It's a tie! " + playerSelection.toLowerCase() + " do not beat " + computerSelection.toLowerCase();
-    } else {
-        return "You Lose! " + computerSelection.toLowerCase() + " beats " + playerSelection.toLowerCase();
-    }
+        if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Scissors' ) {
+            playerScore += 1;
+            console.log("Playerscore = " + playerScore + " and Computerscore " + computerScore)
+            return "You Win! Rock beats Scissors";
+        } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Rock') {
+            playerScore += 1;
+            console.log("Playerscore = " + playerScore + " and Computerscore " + computerScore)
+            return "You Win! Paper beats Rock";
+        } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Paper') {
+            playerScore += 1;
+            console.log("Playerscore = " + playerScore + " and Computerscore " + computerScore)
+            return "You Win! Scissors beats Paper";
+        } else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+            return "It's a tie! " + playerSelection.toLowerCase() + " do not beat " + computerSelection.toLowerCase();
+        } else {
+            computerScore += 1;
+            console.log("Playerscore = " + playerScore + " and Computerscore " + computerScore)
+            return "You Lose! " + computerSelection.toLowerCase() + " beats " + playerSelection.toLowerCase();
+        }
+
+    
 }
-const playerSelection = 'Rock'
-const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection));
+/*function that loops the game while either computer score and pc score is below 5, if it exceeds this value the game stops and returns who won. */
+function game() {
+    while (playerScore < 5 || computerScore < 5) {
+        let playerSelection = prompt("Rock, Paper or Scissors?");
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        if (playerScore == 5 || computerScore == 5) {
+            if (playerScore == 5) {
+                console.log("Player wins!")
+            } else {
+                console.log("Computer wins!")
+            }
+            break;
+        } 
+    }   
+}
